7cc89b6ceb687fbdfc89655bf1079d2a169096a6
Kasun Hewage <kasun.ch@gmail.com>
2020-12-29 15:35:48 +0100

    Initial files

---
+++ conanfile.py
@@ -0,0 +1,69 @@
+import re
+from conans import ConanFile, CMake, tools
+
+
+class LibQZmqConan(ConanFile):
+    name = "libqzmq"
+    description = "Qt library for ZMQ"
+    url = "https://github.com/kasunch/libqzmq"
+    homepage = "https://github.com/kasunch/libqzmq"
+    license = "Apache-2.0"  # SPDX Identifiers https://spdx.org/licenses/
+    exports_sources = ["src/*", "qzmqConfig.cmake.in", "CMakeLists.txt"]
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+    }
+    default_options = {
+        "shared": False,
+    }
+
+    _git_is_dirty = False
+    _git_commit = "unknown"
+    _cmake = None
+
+    def set_version(self):
+        git = tools.Git(folder=self.recipe_folder)
+        if not self.version:
+            output = git.run("describe --all").splitlines()[0].strip()
+            self.version = re.sub("^.*/v?|^v?", "", output)
+        output = git.run("diff --stat").splitlines()
+        self._git_is_dirty = True if output else False
+        self._git_commit = git.run("rev-parse HEAD").splitlines()[0].strip()
+
+        self.output.info("Version: %s, Commit: %s, Is_dirty: %s" %
+                         (self.version, self._git_commit, self._git_is_dirty))
+
+    def requirements(self):
+        self.requires("qt/5.14.2@bincrafters/stable")
+        self.requires("zeromq/4.3.3")
+
+    def _configure_cmake(self):
+        if not self._cmake:
+            self._cmake = CMake(self)
+            if self.options.shared:
+                self._cmake.definitions["BUILD_SHARED"] = "ON"
+                self._cmake.definitions["BUILD_STATIC"] = "OFF"
+            else:
+                self._cmake.definitions["BUILD_SHARED"] = "OFF"
+                self._cmake.definitions["BUILD_STATIC"] = "ON"
+            if self.options["zeromq"].shared:
+                self._cmake.definitions["ZMQ_SHARED"] = "ON"
+            else:
+                self._cmake.definitions["ZMQ_SHARED"] = "OFF"
+            self._cmake.definitions["USE_CONAN_BUILD_INFO"] = "ON"
+            self._cmake.definitions["SOURCE_VERSION"] = self.version
+            self._cmake.definitions["SOURCE_COMMIT"] = self._git_commit
+            self._cmake.definitions["SOURCE_DIRTY"] = self._git_is_dirty
+            del self._cmake.definitions["CMAKE_EXPORT_NO_PACKAGE_REGISTRY"]
+            self._cmake.configure()
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        pass
+
+    def package_info(self):
+        pass
---
+++ src/qzmqcommon.hpp
@@ -0,0 +1,54 @@
+// Copyright 2019 Kasun Hewage
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __QZMQ_COMMON_H__
+#define __QZMQ_COMMON_H__
+
+// Handle DSO symbol visibility
+// Refer https://gcc.gnu.org/wiki/Visibility for more details
+
+// Generic helper definitions for shared library support
+#if defined _WIN32 || defined __CYGWIN__
+#define QZMQ_HELPER_DLL_IMPORT __declspec(dllimport)
+#define QZMQ_HELPER_DLL_EXPORT __declspec(dllexport)
+#define QZMQ_HELPER_DLL_LOCAL
+#else // defined _WIN32 || defined __CYGWIN__
+#if __GNUC__ >= 4
+#define QZMQ_HELPER_DLL_IMPORT __attribute__ ((visibility ("default")))
+#define QZMQ_HELPER_DLL_EXPORT __attribute__ ((visibility ("default")))
+#define QZMQ_HELPER_DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+#else // __GNUC__ >= 4
+#define QZMQ_HELPER_DLL_IMPORT
+#define QZMQ_HELPER_DLL_EXPORT
+#define QZMQ_HELPER_DLL_LOCAL
+#endif // __GNUC__ >= 4
+#endif // defined _WIN32 || defined __CYGWIN__
+
+// Now we use the generic helper definitions above to define QZMQ_API and QZMQ_LOCAL.
+// QZMQ_API is used for the public API symbols. 
+// It either DLL imports or DLL exports (or does nothing for static build).
+// QZMQ_LOCAL is used for non-api symbols.
+#ifdef QZMQ_DLL
+#ifdef QZMQ_DLL_EXPORTS
+#define QZMQ_API QZMQ_HELPER_DLL_EXPORT
+#else // QZMQ_DLL_EXPORTS
+#define QZMQ_API QZMQ_HELPER_DLL_IMPORT
+#endif // QZMQ_DLL_EXPORTS
+#define QZMQ_LOCAL QZMQ_HELPER_DLL_LOCAL
+#else // QZMQ_DLL
+#define QZMQ_API
+#define QZMQ_LOCAL
+#endif // QZMQ_DLL
+
+#endif // __QZMQ_COMMON_H__
